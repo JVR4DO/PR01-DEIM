fc5d1d8a2422d4e617e971aa58461827e9171459
Sergio <Sergiojurado111@gmail.com>
2021-09-17 17:19:51 +0200

    Ejercicio 1a
    Declaro variables y pongo valores

---
+++ Ejercicio 01 Sergio Jurado/Assets/Ejercicios.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicios : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+   int myInt = 5;
+   float myFloat = 5.3f;
+   string myString = "Hola Guapos";
+   bool myBool = true;
+            
+    void Start()
+    {
+
+        print(myInt);
+        print(myFloat);
+        print(myString);
+        print(myBool);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
