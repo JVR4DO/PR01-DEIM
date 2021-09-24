f4fe757fc960a59b3d66f36f7326b12f768b34ca
Sergio <Sergiojurado111@gmail.com>
2021-09-24 17:22:19 +0200

    Contador Para y Empieza con la misma tecla

--- Ejercicio 01 Sergio Jurado/Assets/Contador.cs
+++ Ejercicio 01 Sergio Jurado/Assets/Contador.cs
@@ -8,6 +8,7 @@ public class Contador : MonoBehaviour
     [SerializeField] Text Mytext;
     [SerializeField] float speed;
     int n;
+    bool contadorencendido = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,13 +29,19 @@ public class Contador : MonoBehaviour
     }
     void Update()
     {
-      if(Input.GetButtonDown ("Fire1"))
+        if (Input.GetButtonDown("Fire1"))
         {
-            StartCoroutine("Cronometro");
-        }
-        if(Input.GetButtonUp("Fire2"))
-        {
-            StopCoroutine("Cronometro");
+            if (contadorencendido == false)
+            {
+                StartCoroutine("Cronometro");
+                contadorencendido = true;
+            }
+
+            else
+            {
+                StopCoroutine("Cronometro");
+                contadorencendido = false;
+            }
         }
     }
 }
